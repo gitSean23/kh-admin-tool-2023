ecdbd21dc1445f5845858db5758557234cb6b264
Caleb Rivera <calebrossr@gmail.com>
2023-04-07 18:04:57 -0400

    Adjust header styles

--- src/app/(dashboard)/(data)/Header.tsx
+++ src/app/(dashboard)/(data)/Header.tsx
@@ -5,29 +5,29 @@ export function Header() {
     <div className="flex h-36 items-center justify-between whitespace-nowrap border-b bg-[#fbfbfb] px-10">
       <div>
         <div className="text-3xl font-bold text-[#4b4b4b]">Fall 2022</div>
-        <div className="text-xl font-medium text-[#8d8d8d]">March 25-26</div>
+        <div className="text-xl font-semibold text-[#8d8d8d]">March 25-26</div>
       </div>
       <div className="flex gap-4">
         <Link
           href="/attendees"
-          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white p-6 text-left"
+          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white px-5 py-6  text-left"
         >
           <div className="text-4xl font-bold text-[#4b4b4b]">0</div>
-          <div className="text-2xl font-medium text-[#8d8d8d]">Attendees</div>
+          <div className="text-2xl font-semibold text-[#8d8d8d]">Attendees</div>
         </Link>
         <Link
           href="/sponsors"
-          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white p-6 text-left"
+          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white px-5 py-6  text-left"
         >
           <div className="text-4xl font-bold text-[#4b4b4b]">0</div>
-          <div className="text-2xl font-medium text-[#8d8d8d]">Sponsors</div>
+          <div className="text-2xl font-semibold text-[#8d8d8d]">Sponsors</div>
         </Link>
         <Link
           href="/events"
-          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white p-6 text-left"
+          className="flex h-24 w-52 flex-col justify-center rounded-lg border bg-white px-5 py-6  text-left"
         >
           <div className="text-4xl font-bold text-[#4b4b4b]">0</div>
-          <div className="text-2xl font-medium text-[#8d8d8d]">Events</div>
+          <div className="text-2xl font-semibold text-[#8d8d8d]">Events</div>
         </Link>
       </div>
     </div>
